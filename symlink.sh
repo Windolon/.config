4c696ec4412a4f6803218ec96d3613f93c1948d3
Windolon <windyscapexe@gmail.com>
2025-12-18 23:44:42 +0800

    general: remove the symlink script

--- symlink.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-ln -s ~/.rc/alacritty ~/.config/alacritty
-ln -s ~/.rc/conda ~/.config/conda
-ln -s ~/.rc/nvim ~/.config/nvim
-ln -s ~/.rc/tmux ~/.config/tmux
-ln -s ~/.rc/zsh/zshrc ~/.zshrc
